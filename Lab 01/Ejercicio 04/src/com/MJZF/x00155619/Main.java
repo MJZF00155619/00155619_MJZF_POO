4757ac42f17b4d81b62b47d9463aac00543f6513
MJZF00155619 <00155619@uca.edu.sv>
2020-03-16 17:53:10 -0600

    Add exercise 01,02,03,04,05 lab 01

--- Lab 01/Ejercicio 04/src/com/MJZF/x00155619/Main.java
+++ Lab 01/Ejercicio 04/src/com/MJZF/x00155619/Main.java
@@ -3,35 +3,32 @@ package com.MJZF.x00155619;
 import java.util.Scanner;
 
 public class Main {
-
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
-        char [] caracteres;
-        int ini, fin = 0;
-        boolean palindromo;
         System.out.print("Ingrese la palabra o frase a verificar si es palindromo: ");
         String palabra = entrada.nextLine();
         palabra = palabra.toLowerCase();
-        palabra = palabra.replace(" ","");
+        palabra = palabra.replace(" ", "");
+
+        System.out.println(palindromo(palabra));
+
+    }
+
+    public static boolean palindromo(String palabra) {
+        char[] caracteres;
+        int ini, fin = 0;
         caracteres = palabra.toCharArray();
         ini = 0;
         fin = caracteres.length - 1;
 
-        while(ini<fin){
-            if(caracteres[ini]==caracteres[fin]){
+        while (ini < fin) {
+            if (caracteres[ini] == caracteres[fin]) {
                 ini++;
                 fin--;
-            }
-            else {
-                palindromo=false;
-                System.out.println(palindromo);
-                break;
+            } else {
+                return false;
             }
         }
-        if(ini==fin){
-            palindromo=true;
-            System.out.println(palindromo);
-        }
-
+            return true;
     }
 }
