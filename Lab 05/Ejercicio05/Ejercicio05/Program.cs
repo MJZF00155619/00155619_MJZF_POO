95dd5e9386a5f2e66bdb0abe7071cb6b264cd561
MJZF00155619 <00155619@uca.edu.sv>
2020-04-22 17:51:52 -0600

    Add: exercise 05 C#

---
+++ Lab 05/Ejercicio05/Ejercicio05/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Ejercicio05
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            string cadena;
+            char cambiar;
+            char reemplazo;
+            Console.WriteLine("Escriba la cadena de texto a modificar: ");
+            cadena = Console.ReadLine();
+            Console.WriteLine("Ingrese el caracter que desea reemplazar: ");
+            cambiar = Convert.ToChar(Console.ReadLine());
+            Console.WriteLine("¿Con que caracter desea reemplazarlo? ");
+            reemplazo = Convert.ToChar(Console.ReadLine());
+            Console.WriteLine(cadena.Replace(cambiar, reemplazo));
+        }
+    }
+}
