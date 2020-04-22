6fecbb20810190baf2883bd1d4262f6b2f7870dc
MJZF00155619 <00155619@uca.edu.sv>
2020-04-22 17:45:36 -0600

    Add : Exercise Lab1 C#

---
+++ Lab 05/Ejercicio01/Ejercicio01/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Ejercicio01
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            int num = 0, anterior = 0, pos = 1;
+
+
+            Console.WriteLine("Ingrese cuantos terminos Fibonacci desea: ");
+            int numero = Convert.ToInt32(Console.ReadLine());
+            
+            for (int i = 0; i < numero; i++)
+            {
+                anterior = pos;
+                pos = num;
+                num = anterior + pos;
+
+                Console.Write(num + " ");
+            }
+        }
+    }
+}
