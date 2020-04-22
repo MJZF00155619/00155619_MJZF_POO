9e36c90fe80b62476396c73560bc90eaea977cc0
MJZF00155619 <00155619@uca.edu.sv>
2020-04-22 17:47:46 -0600

    Add Exercise02 C#

---
+++ Lab 05/Ejercicio02/Ejercicio02/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Ejercicio02
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese el valor de n: ");
+            byte n = Convert.ToByte(Console.ReadLine());
+            int i = 1;
+
+            if (i < n)
+            {
+                for (i = 1; i < n; i++) {
+                    Console.Write(i+" ");
+                }
+            }
+            for(i=n; i>0; i--){
+               Console.Write(i+" ");
+            }
+        }
+    }
+}
