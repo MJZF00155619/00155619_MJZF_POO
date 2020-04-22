2189838c1f407322acde3e2d78b2192912d77e50
MJZF00155619 <00155619@uca.edu.sv>
2020-04-22 17:50:44 -0600

    Add: exercise 04 C#

---
+++ Lab 05/Ejercicio04/Ejercicio04/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Ejercicio04
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese la palabra o frase a verificar si es palindromo: ");
+            string palabra = Console.ReadLine();
+            palabra = palabra.Replace(" ","");
+            palabra = palabra.ToLower();
+
+            Console.WriteLine(palindromo(palabra));
+        }
+        
+        public static bool palindromo(String palabra) {
+            char[] caracteres;
+            int ini, fin = 0;
+            caracteres = palabra.ToCharArray();
+            ini = 0;
+            fin = caracteres.Length - 1;
+
+            while (ini < fin) {
+                if (caracteres[ini] == caracteres[fin]) {
+                    ini++;
+                    fin--;
+                } else {
+                    return false;
+                }
+            }
+            return true;
+        }
+        
+    }
+}
