d4911d60cc58965eb0cd86fd1f4a8dd17ca3a9a9
MJZF00155619 <00155619@uca.edu.sv>
2020-04-23 22:00:33 -0600

    Add: meet exercise folder

---
+++ Ejercicios Meet/Meet2304/Meet2304/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Meet2304
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var p = new List<Persona>();
+            
+            p.Add(new Persona("Mauricio","Zaldaña",18));
+            p.Add(new Persona("Mauricio",edad:18));
+            p.Add(new Persona(edad: 18));
+            p.Add(new Persona(apellido: "Zaldaña"));
+
+            String buscar = "Mauricio";
+            buscar = Console.ReadLine();
+
+            p.ForEach(it =>
+            {
+                if (buscar.Equals(it.Nombre))
+                {
+                    Console.WriteLine("Nuevo nombre: ");
+                    it.Nombre = Console.ReadLine();
+                    Console.WriteLine(it.ToString());
+                }
+            });
+        }
+    }
+}
