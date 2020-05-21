ed4c204c53ec6f18315f2a42b68686dbb10d3cfd
MJZF00155619 <00155619@uca.edu.sv>
2020-05-20 21:50:02 -0600

    Add: RegisterStudent UserControl

---
+++ Lab 09/Labo09/Labo09/RegisterStudent.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Forms;
+
+namespace Labo09
+{
+    public partial class RegisterStudent : UserControl
+    {
+        public RegisterStudent()
+        {
+            InitializeComponent();
+        }
+    }
+}
