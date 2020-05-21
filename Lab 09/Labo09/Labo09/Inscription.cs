8d028ed8962a685016f6bac99abf167775655ec7
MJZF00155619 <00155619@uca.edu.sv>
2020-05-20 22:40:22 -0600

    Add: Inscription UserControl

---
+++ Lab 09/Labo09/Labo09/Inscription.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Forms;
+
+namespace Labo09
+{
+    public partial class Inscription : UserControl
+    {
+        public Inscription()
+        {
+            InitializeComponent();
+        }
+    }
+}
