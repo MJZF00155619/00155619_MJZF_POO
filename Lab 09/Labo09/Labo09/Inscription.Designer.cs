31084c2dd1fddd25271b40ea79e6349556ea5641
MJZF00155619 <00155619@uca.edu.sv>
2020-05-20 23:15:51 -0600

    Modify ViewData UserControl

--- Lab 09/Labo09/Labo09/Inscription.Designer.cs
+++ Lab 09/Labo09/Labo09/Inscription.Designer.cs
@@ -42,6 +42,7 @@ namespace Labo09
             // 
             // tableLayoutPanel1
             // 
+            this.tableLayoutPanel1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
             this.tableLayoutPanel1.ColumnCount = 2;
             this.tableLayoutPanel1.ColumnStyles.Add(
                 new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 30F));
@@ -52,7 +53,8 @@ namespace Labo09
             this.tableLayoutPanel1.Controls.Add(this.textBox1, 1, 0);
             this.tableLayoutPanel1.Controls.Add(this.button1, 0, 2);
             this.tableLayoutPanel1.Controls.Add(this.comboBox1, 1, 1);
-            this.tableLayoutPanel1.Location = new System.Drawing.Point(5, 6);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
             this.tableLayoutPanel1.RowCount = 3;
             this.tableLayoutPanel1.RowStyles.Add(
@@ -61,15 +63,15 @@ namespace Labo09
                 new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
             this.tableLayoutPanel1.RowStyles.Add(
                 new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
-            this.tableLayoutPanel1.Size = new System.Drawing.Size(428, 279);
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(444, 297);
             this.tableLayoutPanel1.TabIndex = 0;
             // 
             // label2
             // 
             this.label2.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.label2.Location = new System.Drawing.Point(3, 93);
+            this.label2.Location = new System.Drawing.Point(3, 99);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(122, 93);
+            this.label2.Size = new System.Drawing.Size(127, 99);
             this.label2.TabIndex = 4;
             this.label2.Text = "Materia";
             this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -79,7 +81,7 @@ namespace Labo09
             this.label1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.label1.Location = new System.Drawing.Point(3, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(122, 93);
+            this.label1.Size = new System.Drawing.Size(127, 99);
             this.label1.TabIndex = 0;
             this.label1.Text = "Carnet";
             this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -87,7 +89,7 @@ namespace Labo09
             // textBox1
             // 
             this.textBox1.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.textBox1.Location = new System.Drawing.Point(147, 35);
+            this.textBox1.Location = new System.Drawing.Point(157, 38);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(262, 23);
             this.textBox1.TabIndex = 1;
@@ -97,9 +99,9 @@ namespace Labo09
             this.button1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
             this.tableLayoutPanel1.SetColumnSpan(this.button1, 2);
             this.button1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.button1.Location = new System.Drawing.Point(3, 189);
+            this.button1.Location = new System.Drawing.Point(3, 201);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(422, 87);
+            this.button1.Size = new System.Drawing.Size(438, 93);
             this.button1.TabIndex = 2;
             this.button1.Text = "Inscribir";
             this.button1.UseVisualStyleBackColor = true;
@@ -108,7 +110,7 @@ namespace Labo09
             // 
             this.comboBox1.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(146, 124);
+            this.comboBox1.Location = new System.Drawing.Point(156, 137);
             this.comboBox1.Name = "comboBox1";
             this.comboBox1.Size = new System.Drawing.Size(264, 23);
             this.comboBox1.TabIndex = 5;
