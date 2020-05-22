1ad26929b95aafccb74bbbb4f082e827cc38ad73
MJZF00155619 <00155619@uca.edu.sv>
2020-05-22 12:57:39 -0600

    Rename to RegisterStudent

--- Lab9/Lab9/Lab9/AddSignature.Designer.cs
+++ Lab9/Lab9/Lab9/AddSignature.Designer.cs
@@ -25,14 +25,21 @@ namespace Lab9
 
         #region Component Designer generated code
 
-        /// <summary> 
-        /// Required method for Designer support - do not modify 
+        /// <summary>
+        /// Required method for Designer support - do not modify
         /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // AddSignature
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Name = "AddSignature";
+            this.Size = new System.Drawing.Size(480, 344);
+            this.ResumeLayout(false);
         }
 
         #endregion
