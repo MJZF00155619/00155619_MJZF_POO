93e687798865f394a785e447698edc06dcf8b712
MJZF00155619 <00155619@uca.edu.sv>
2020-06-09 20:48:19 -0600

    Modify Action and Add exception

--- Lab10/Lab10/Program.cs
+++ Lab10/Lab10/Program.cs
@@ -10,8 +10,12 @@ namespace Lab10
          static MyDelegate del;
         public static void Main(string[] args)
         {
+            
             int option;
             var Propietarios = new List<CuentaBancaria>();
+            Action<List<CuentaBancaria>> myAction;
+            Action<List<CuentaBancaria>> myAction2;
+            Action<List<CuentaBancaria>> myAction3;
             string nombre;
             double saldo;
             bool continuar = true;
@@ -20,43 +24,52 @@ namespace Lab10
                 
                 Console.WriteLine(
                     "Ingrese la opcion que desea \n 1. Agregar cuenta \n 2.Ver cuentas almacenadas \n 3. Ver cuentas y total de las cuentas \n 4.Ver cuentas, total de cuentas y cuentas de personas cuyo nombre empieza en vocal \n 5. Salir");
-                option = Convert.ToInt16(Console.ReadLine());
-
-                switch (option)
+                try
                 {
-                    case 1 : 
-                        Console.WriteLine("Ingrese el nombre: ");
-                        nombre = Console.ReadLine();
-                        Console.WriteLine("Ingrese el saldo actual");
-                        saldo = Convert.ToDouble(Console.ReadLine());
-                        
-                        Propietarios.Add(new CuentaBancaria(nombre,saldo));
-                        break;
-                    case 2 :
-                        del = new MyDelegate(VerCuentas);
-                        del(Propietarios);
-                        break;
-                    case 3 :
-                        del = new MyDelegate(VerCuentas);
-                        del(Propietarios);
-                        del = new MyDelegate(TotalCuentas);
-                        del(Propietarios);
-                        
-                        break;
-                    case 4 :
-                        del = new MyDelegate(VerCuentas);
-                        del(Propietarios);
-                        del = new MyDelegate(TotalCuentas);
-                        del(Propietarios);
-                        del = new MyDelegate(Vocal);
-                        del(Propietarios);
-                        break;
-                    case 5 :
-                        continuar = false;
-                        break;
-                    default:
-                        Console.WriteLine("Numero fuera del rango permitido");
-                        break;
+                option = Convert.ToInt16(Console.ReadLine());
+                
+                    switch (option)
+                    {
+                        case 1:
+                            Console.WriteLine("Ingrese el nombre: ");
+                            nombre = Console.ReadLine();
+                            Console.WriteLine("Ingrese el saldo actual");
+                            saldo = Convert.ToDouble(Console.ReadLine());
+
+                            Propietarios.Add(new CuentaBancaria(nombre, saldo));
+                            break;
+                        case 2:
+                            del = new MyDelegate(VerCuentas);
+                            del(Propietarios);
+                            break;
+                        case 3:
+                            del = new MyDelegate(VerCuentas);
+                            del(Propietarios);
+                            del = new MyDelegate(TotalCuentas);
+                            del(Propietarios);
+
+                            break;
+                        case 4:
+                            myAction2 = VerCuentas;
+                            myAction3 = TotalCuentas;
+                            myAction = Vocal;
+                            
+                            myAction2.Invoke(Propietarios);
+                            myAction3.Invoke(Propietarios);
+                            myAction.Invoke(Propietarios);
+                            break;
+                        case 5:
+                            continuar = false;
+                            break;
+                        default:
+                            Console.WriteLine("Numero fuera del rango permitido");
+                            break;
+                    }
+                }
+                catch (System.FormatException)
+                {
+                    Console.WriteLine("Error, ha ingresado un formato invalido");
+                    option = 0;
                 }
 
             } while (continuar);
